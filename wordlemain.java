07f9a2438e891dc934f6dbe63209cfe8c4434128
kobasaurus <101825592+kobasaurus@users.noreply.github.com>
2022-05-27 12:42:28 -0400

    Update wordlemain.java

--- wordlemain.java
+++ wordlemain.java
@@ -3,14 +3,10 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class MainJava extends JPanel
+public class wordlemain extends JPanel
 {
     public static void main(String[]args){
         System.out.print('\f');
         Scanner sc = new Scanner(System.in);
     }
 }
-
-
-
-
