20af49f2c0396e1d3aace81e2b857d9431c2e14c
James Begin <103123677+James-Begin@users.noreply.github.com>
2022-05-30 14:55:49 -0400

    Update testcode.java

--- testcode.java
+++ testcode.java
@@ -4,57 +4,88 @@ import java.util.Arrays;
 
 public class wordletest {
     public static void main(String[] args) {
+        //Initialize the wordlist, containing only 5-letter words
         String[] wordlist = {"cheek", "apple", "heart", "knead", "smell"};
-        int word = (int)(Math.random() * 5);
-        String autoword = "apple";
-        //convert chosen string to an arraylist for comparison
-        //split whenever there is no space
-        String[] splitString = autoword.split("");
+        //Choose a random word from the wordlist by choosing a random index within the word list 
+        // and choosing the word assigned to that index
+        int word = (int)(Math.random() * wordlist.length);
+        String autoword = wordlist[word];
         
+        
+        //convert chosen string to an arraylist in order to compare each letter and to print it in the GUI
+        //use the split function to split when there is no space character in the string, then print it
+        String[] splitString = autoword.split("");
         print(splitString);
+        
+        //Initialize the word the user guesses
+        String guess = "";
+        
+        //initialize a scanner and ask the user for an input. If
         Scanner sc = new Scanner(System.in);
-        System.out.println("guess a word");
-        String guess = sc.nextLine();
-        if (guess.length() == 5) {
-            print(split(guess));
+        while (guess.length() != 5) {
+            System.out.println("guess a word");
+            guess = sc.nextLine();
         }
+        
         
         String[] chosen = split(autoword);
-        System.out.println(chosen[0]);
+        
         String[] guessnew = split(guess);
-        System.out.println(guessnew[0]);
-        if (chosen[0] == guessnew[0]) {
-            System.out.println("green");
-        }
+        print(guessnew);
+        
+
+        
         System.out.println("\n");
-        // print(check(chosen, guessnew));
+        printInt(check(chosen, guessnew));
     }
     public static void print(String[] words) {
+    
         for (int i = 0; i < words.length;i++) {
             System.out.print(words[i] + " ");
+            }
+        
+        
+    }
+    public static void printInt(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
         }
     }
+    
+    //split function uses the .split() String method to split a string into a String array 
+    //where each index is a seperate letter in the string
+    //In this case, we split when there is no space character as every input is a single word
     public static String[] split(String word) {
         String[] splitString = word.split("");
         return splitString;
     }
-    public static String[] check(String[] autoword, String[] guess) {
-        String[] ans = new String[autoword.length];
+    public static int[] check(String[] autoword, String[] guess) {
+        int[] ans = new int[autoword.length];
+        
         for (int i = 0; i < autoword.length; i++) {
             
-            if (autoword[i] == guess[i]) {
-                System.out.print(autoword[i] + " " + guess[i]);
-                ans[i] = "green";
+            if (autoword[i].equals(guess[i])) {
+                
+                ans[i] = 2;
+                continue;
+            } else if (letter_In_String(autoword, guess, i)) {
+                ans[i] = 1;
+            } else {
+                ans[i] = 0;
             }
-            for (int k = 0; k < autoword.length; k++) {
-                if (guess[i] == autoword[k] && i != k) {
-                    ans[i] = "yellow";
-                }
-            }
-            if (ans[i] == "") {
-                ans[i] = "grey";
-            }
+            
+            
         }
         return ans;
+    }
+    public static boolean letter_In_String(String[] autoword, String[] guess, int i) {
+        for (int k = 0; k < autoword.length; k++) {
+                if (guess[i].equals(autoword[k]) && i != k) {
+                    return true;
+                    
+                }
+                
+        }
+        return false;
     }
 }
