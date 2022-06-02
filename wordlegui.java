1297c94bba817a6866c7e5807f9ce20846d70ebe
kobasaurus <101825592+kobasaurus@users.noreply.github.com>
2022-06-02 13:20:45 -0400

    Update wordlegui.java

--- wordlegui.java
+++ wordlegui.java
@@ -3,12 +3,11 @@ import java.util.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import java.awt.Dimension;
 
 
 public class wordlegui extends JPanel 
 {   // create label for text "Make a Guess"
-    static Label l1 = new Label("");
+    static Label l1  = new Label("");
     // create label for text "Please guess a 5 letter word"
     static Label l2 = new Label("");
     // create label for previous guesses text
@@ -25,8 +24,8 @@ public class wordlegui extends JPanel
     // make button to submit guess
     static Button b1 = new Button("Guess!");
     
-    // create list for if the guesses are correct or not
-    static int[] guess = {1,1,1,2,2};
+    // create list to store the guess answers
+    static int[] guessCheck = {1,1,1,2,2};
     
     // store previous answers of if the guesses are correct
     static int[] row1ans = {0,0,0,0,0};
@@ -35,18 +34,24 @@ public class wordlegui extends JPanel
     static int[] row4ans = {0,0,0,0,0};
     static int[] row5ans = {0,0,0,0,0};
     
+    // show the previous guesses
     static String[] preguess = {"","","","",""};
     
+    // placeholder text for adding labels
     static String placeText = "";
     
-    // create variable for what row to run at
+    // create variable for what row to draw boxes at
     static int row = 1;
+    
+    // create variable to store the guess
+    static String guess;
     
     static wordlegui gui = new wordlegui();
     static JFrame frame = new JFrame("Wordle");
     static Scanner sc = new Scanner(System.in);
+    
     public static void main(String[]args){
-       // add all elements to frame
+        // add all elements to frame
         frame.add(l1);
         frame.add(l2);
         frame.add(l3);
@@ -57,52 +62,44 @@ public class wordlegui extends JPanel
         frame.add(l8);
         frame.add(t1);
         frame.add(b1);
+        // implement button code
+        b1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                // delegate to event handler method
+                buttonGuessAction(evt);
+            }
+        });
         
         // set frame size and make it visible
         frame.setSize(1000,1000);
         frame.add(gui);
         frame.setVisible(true);
-        
         // wait for guess one
         int x = sc.nextInt();
         row1ans = guess(1,row1ans);
-        
         // wait for guess two
         x = sc.nextInt();
         row2ans = guess(2,row2ans);
-        
         // wait for guess three
         x = sc.nextInt();
         row3ans = guess(3,row3ans);
-        
         // wait for guess four
         x = sc.nextInt();
         row4ans = guess(4,row4ans);
-        
         // wait for final guess
         x = sc.nextInt();
         row5ans = guess(5,row5ans);
     }
-    public static void addlabel(Label l, String text, int x, int y, int sizex, int sizey, int fontsize){
-        l.setBounds(x,y,sizex,sizey);
-        l.setText(text);
-        l.setFont(new Font("Serif", Font.PLAIN, fontsize));
-    }
+    
     public void paint (Graphics g){
         // add label for make a guess
         placeText = "Make A Guess";
-        addlabel(l1,placeText,350,10,500,50,50);
         l1.setForeground(Color.BLUE);
+        addlabel(l1,placeText,350,10,500,50,50);
         
-        // add label for please guess a five letter word
-        placeText = "Please Guess a Five Letter Word!!!";
-        addlabel(l2,placeText,375,800,500,50,20);
-        l2.setForeground(Color.RED);
-        
-        l3.setBounds(10,200,200,25);
+        // add label for the previous answers list
         placeText="Previous Guess:";
-        l3.setFont(new Font("Serif", Font.PLAIN, 30));
-        l3.setForeground(Color.GREEN);
         addlabel(l3,placeText,10,200,200,25,30);
         // add text box for user to input guess
         t1.setBounds(400,700,200,25);
@@ -123,13 +120,28 @@ public class wordlegui extends JPanel
         row = row + 1;
         nextRow(row5ans, g);
         row = 1;
+        
+        preGuess();
+    }    
+    
+    public static void buttonGuessAction(ActionEvent evt){
+        guess = t1.getText();
+        System.out.print(guess);
+        if(guess.length() > 5 | guess.length() < 5){
+            frame.add(l2);
+            // add label for please guess a five letter word
+            placeText = "Please Guess a Five Letter Word!!!";
+            l2.setForeground(Color.RED);
+            addlabel(l2,placeText,375,800,500,50,20);
+        } else{
+            frame.remove(l2);
+        }
     }
+    
     public static void nextRow(int[] letters, Graphics g){
         // this block will create the answer squares with the correct 
         // information
         for(int i = 0; i<=400; i=i+100){
-            
-            
             if(letters[i/100] == 2){
                 // if the letter is in the right spot and the right letter
                 // make the box green
@@ -142,54 +154,56 @@ public class wordlegui extends JPanel
                 // if neither is correct, keep the box gray
                 g.setColor(Color.gray);
             }
-            
-            // add the box
+            // draw the box
             g.fillRect((250+i),(100*row),90,90);
-            
         }
-        
-        l4.setBounds(10,230,150,15);
-        l4.setText(preguess[0]);
-        l4.setFont(new Font("Serif", Font.PLAIN, 20));
+    }
+    // add the previous guesses
+    public static void preGuess(){
+        // set label colors as red
         l4.setForeground(Color.RED);
-        
-        l5.setBounds(10,250,150,15);
-        l5.setText(preguess[1]);
-        l5.setFont(new Font("Serif", Font.PLAIN, 20));
         l5.setForeground(Color.RED);
-        
-        l6.setBounds(10,270,150,15);
-        l6.setText(preguess[2]);
-        l6.setFont(new Font("Serif", Font.PLAIN, 20));
         l6.setForeground(Color.RED);
-        
-        l7.setBounds(10,290,150,15);
-        l7.setText(preguess[3]);
-        l7.setFont(new Font("Serif", Font.PLAIN, 20));
         l7.setForeground(Color.RED);
-        
-        l8.setBounds(10,310,150,15);
-        l8.setText(preguess[4]);
-        l8.setFont(new Font("Serif", Font.PLAIN, 20));
         l8.setForeground(Color.RED);
         
+        // add the previous guesses
+        placeText=preguess[0];
+        addlabel(l4,placeText,10,230,150,15,20);
+        
+        placeText=preguess[1];
+        addlabel(l5,placeText,10,250,150,15,20);
+        
+        placeText=preguess[2];
+        addlabel(l6,placeText,10,270,150,15,20);
+        
+        placeText=preguess[3];
+        addlabel(l7,placeText,10,290,150,15,20);
+        
+        placeText=preguess[4];
+        addlabel(l8,placeText,10,310,150,15,20);
     }
     
+    // logic for next guess
     public static int[] guess(int guessnum, int[] rowans){
-        rowans = remap(guess);
-        preguess[guessnum-1] = "WWWWW";
+        rowans = remap(guessCheck);
+        preguess[guessnum-1] = "WWMWW";
         frame.add(gui);
         SwingUtilities.updateComponentTreeUI(frame);
         return rowans;
     }
-    
+    // take the guess and store it in the answer log
     public static int[] remap(int[] guess){
-        
-        // take the guess and store it in the answer log
         int[] ans = {0,0,0,0,0};
         for(int i = 0; i < 5; i++){
             ans[i] = guess[i];
         }
         return ans;
+    }
+    // method for adding labels
+    public static void addlabel(Label l, String text, int x, int y, int sizex, int sizey, int fontsize){
+        l.setBounds(x,y,sizex,sizey);
+        l.setText(text);
+        l.setFont(new Font("Serif", Font.PLAIN, fontsize));
     }
 }
