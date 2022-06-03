d78639f92d92cc297f83709bb00e95578d2885ec
kobasaurus <101825592+kobasaurus@users.noreply.github.com>
2022-06-03 14:54:41 -0400

    Update wordlemain.java

--- wordlemain.java
+++ wordlemain.java
@@ -2,7 +2,10 @@ public class wordlemain
 {
     public static void main(String[]args){
         System.out.print('\f');
+        //create instance of wordlegui and wordlelogic
         wordlegui gui = new wordlegui();
-        wordlelogic logic = new wordlelogic();
+        //wordlelogic logic = new wordlelogic();
+        
+        wordlegui.creation("words");
     }
 }
